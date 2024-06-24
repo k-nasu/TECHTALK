75cd89994c59bc0f07f42da73e5751bf4ae53309
kenta.nasu <46446368+k-nasu@users.noreply.github.com>
2024-06-24 18:50:07 +0900

    add top page idea comment out

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,24 +26,30 @@ export default function Home({ posts }: Props) {
         ここにタグの一覧を出す
       </aside>
       <main className="container lg:basis-2/4 lg:mx-5 px-4 pt-16">
-        <h3 className="mb-5">トレンドの記事</h3>
-        {posts.map(post => (
-          <div key={post.id}>
-            <SinglePost
-              id={post.id}
-              title={post.title}
-              description={post.description}
-              updated_on={post.updated_on}
-              slug={post.slug}
-              tags={post.tags}
-              isPaginationPage={false}
-            />
-          </div>
-        ))}
-        <Link href="/posts/page/1" className="text-blue-600 mb-20 mx-auto pt-5 px-5 block text-right">一覧を見る</Link>
+        <section>
+          <h3 className="mb-5">トレンドの記事</h3>
+          {posts.map(post => (
+            <div key={post.id}>
+              <SinglePost
+                id={post.id}
+                title={post.title}
+                description={post.description}
+                updated_on={post.updated_on}
+                slug={post.slug}
+                tags={post.tags}
+                isPaginationPage={false}
+              />
+            </div>
+          ))}
+          <Link href="/posts/page/1" className="text-blue-600 mb-20 mx-auto pt-5 px-5 block text-right">一覧を見る</Link>
+        </section>
+        <section>
+          {/* ・別のまとめ方を出す
+            例：技術ジャンルごと（Rails特集・Typscript特集）、個人開発の各フェーズ（初学者向け、中級者・上級者）、個人開発、ポートフォリオ、分野別（デザイン、インフラ、フロント、バックエンド） */}
+        </section>
       </main>
       <aside className="lg:basis-1/4 px-4 pt-16">
-
+        {/* ここに広告を出す */}
       </aside>
     </div>
   );
