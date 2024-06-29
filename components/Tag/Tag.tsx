eea51bf86cdcc3e4de1d97924955427ee38ec39e
kenta.nasu <46446368+k-nasu@users.noreply.github.com>
2024-06-29 17:51:21 +0900

    modify tag text style

--- components/Tag/Tag.tsx
+++ components/Tag/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = (props: Props) => {
   const { tag } = props;
 
   return (
-    <span className="text-indigo-500 hover:text-blue-600 shadow-md shadow-indigo-500/20 rounded-xl px-2 pb-1 mb-2 font-medium mr-2 whitespace-nowrap inline-block">
+    <span className="text-indigo-600 hover:text-blue-600 shadow-md shadow-indigo-500/20 rounded-xl px-2 pb-1 mb-2 font-medium mr-2 whitespace-nowrap inline-block">
       {tag}
     </span>
   )
